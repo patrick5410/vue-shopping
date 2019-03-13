6cd7cc58896063ca0475acb207a676743235ad0e
jacken <jacken0759@163.com>
2019-03-13 22:17:51 +0800

    20190313备份

--- src/views/express/Express.vue
+++ src/views/express/Express.vue
@@ -1,7 +1,7 @@
 <!--快递页面-->
 <template>
-  <div class="express-container" v-html="htmlContent">
-
+  <div class="express-container">
+    <iframe class="iframe-container" src="http://www.kdniao.com/JSInvoke/MSearchResult.aspx?expCode=YTO&expNo=804594624802595017&sortType=DESC&color=rgb(46,114,251)&backUrl=http://www.tdxiov.com/shopping/orderDetail?orderId=TDX20190228132116211"></iframe>
   </div>
 </template>
 
@@ -32,3 +32,19 @@
     }
   }
 </script>
+<style lang="scss" scoped>
+  .express-container{
+    width: 100%;
+    height: 100vh;
+    .iframe-container{
+      width: 100%;
+      height: 100%;
+      position: relative;
+      overflow: hidden;
+      html{
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+</style>
